c0154e0b9e85fad5755684fd08a0f3e98448ab3a
Janina <foo1.janina@chrissx.de>
2026-09-14 17:01:26 +0000

    feat: transform Astro Surreal scripts

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// Do not write code directly here, instead use the `src` folder!
-// Then, use this file to export everything you want your user to access.
-
-import MyComponent from "./src/MyComponent.astro";
-
-export default MyComponent;
+export { default } from "./src/integration.ts";
+export type { Me, Any, SurrealElement, SurrealArray } from "./src/types.ts";
+export { me, any } from "./src/helpers.ts";
